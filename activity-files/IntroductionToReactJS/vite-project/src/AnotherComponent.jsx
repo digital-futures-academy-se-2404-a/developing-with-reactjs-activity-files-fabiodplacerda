f9023d58536a61ecf4543c25314a73c9575347eb
fabiodplacerda <lacerda96@gmail.com>
2024-05-22 16:56:55 +0100

    completed activity 2 and 3

---
+++ activity-files/IntroductionToReactJS/vite-project/src/AnotherComponent.jsx
@@ -0,0 +1,20 @@
+const AnotherComponent = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae,
+        inventore dolores. Dolore quis doloremque asperiores nobis! Numquam
+        velit tempore odit modi laborum deleniti assumenda, delectus quisquam?
+        Facere aliquam aperiam eos!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae,
+        inventore dolores. Dolore quis doloremque asperiores nobis! Numquam
+        velit tempore odit modi laborum deleniti assumenda, delectus quisquam?
+        Facere aliquam aperiam eos!
+      </p>
+    </>
+  );
+};
+
+export default AnotherComponent;
---
+++ activity-files/IntroductionToReactJS/vite-project/src/MyComponent.jsx
@@ -0,0 +1,12 @@
+import AnotherComponent from "./AnotherComponent.jsx";
+
+const MyComponent = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <AnotherComponent />
+    </>
+  );
+};
+
+export default MyComponent;
